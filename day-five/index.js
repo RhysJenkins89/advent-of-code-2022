293504e98ba924b7cd38b82f0bd01742747d5997
Rhys <rhys@rhysjenkins.uk>
2023-09-23 18:57:51 +0100

    Removed unnecessary html file, added comments

--- day-five/index.js
+++ day-five/index.js
@@ -7,3 +7,6 @@ const splitInput = fileData.split("\n");
 splitInput.forEach((line) => {
     console.log(line);
 });
+
+// First, split the input data in order to separate the stack data itself from the instructions.
+// I can do this by spliting the input data by the blank line.
