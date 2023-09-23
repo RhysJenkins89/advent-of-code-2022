2352a72d64b5f328c72290cde654bb0f0f2c1105
Rhys <rhys@rhysjenkins.uk>
2023-09-23 18:43:00 +0100

    How do I even?

--- day-five/index.js
+++ day-five/index.js
@@ -1,18 +1,9 @@
-console.log('Hello from day five!');
-const fs = require('fs');
+console.log("Hello from day five!");
+const fs = require("fs");
 
-const fileData = fs.readFileSync('testData.txt', 'utf-8');
-const splitInput = fileData.split('\n');
+const fileData = fs.readFileSync("testData.txt", "utf-8");
+const splitInput = fileData.split("\n");
 
-let stackArray = [];
-splitInput.forEach((line, index) => {
-	Array.from(line).forEach((letter, indexOfLetter) => {
-		if (new RegExp("[A-Z]").test(letter, indexOfLetter)) {
-			stackArray[indexOfLetter].push(letter);
-		}
-	}) 
+splitInput.forEach((line) => {
+    console.log(line);
 });
-console.log(stackArray)
-
-// The above gives me the letter and, more importantly, the index at which the letter appears in the array 
-// Now poopulate a new array of letters based on the index at which they appear in the initial array
