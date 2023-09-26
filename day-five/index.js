e6a8398d2f60a6fbdfc32ae4dd4c7ce7f2987588
Rhys <rhys@rhysjenkins.uk>
2023-09-26 20:52:19 +0100

    Life is pain

--- day-five/index.js
+++ day-five/index.js
@@ -2,11 +2,12 @@ console.log("Hello from day five!");
 const fs = require("fs");
 
 const fileData = fs.readFileSync("testData.txt", "utf-8");
-const splitInput = fileData.split("\n");
+const splitData = fileData.split("\n");
+console.log(splitData);
 
-splitInput.forEach((line) => {
-    console.log(line);
-});
+// splitInput.forEach((line) => {
+//     console.log(line);
+// });
 
 // First, split the input data in order to separate the stack data itself from the instructions.
 // I can do this by spliting the input data by the blank line.
